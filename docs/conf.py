41d21bab6010b9292dbb528a76d12aa125775ea6
Nicolas M. Thiéry <nthiery@users.sf.net>
2017-10-20 13:55:42 +0200

    Use Sage's theme for the documentation

--- docs/conf.py
+++ docs/conf.py
@@ -111,7 +111,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'sage'
+html_theme = 'sage'
 #html_theme_path = ['themes']
 
 # Theme options are theme-specific and customize the look and feel of a theme
